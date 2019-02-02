511f3789a4971e4dcd2cd09ae87c11b6d290fada
Thomas Kosa <thomaskosa24@gmail.com>
2019-02-01 23:19:45 -0500

    updated tank js files

--- js/Pregame.js
+++ js/Pregame.js
@@ -1,275 +1,431 @@
 class Pregame
 	{
+	constructor(game,height){
+		this.game=game;
 
-	constructor(height)
-		{
 		//List of TankPanels
-		this.panels = [];
+		this.start_panel=new StartPanel("red",this);
+		this.tank_panels = [];
 		this.height = canvas.height;
 		this.width = canvas.width;
 		this.focus = null;
-		this.addStartPanel();
-		this.addPanelToMakePanels();
-		this.updatePanelHorizontals();
+		this.settings = new SettingsPanel(this);
+		this.current_panels = [this.start_panel].concat(this.tank_panels);
+			
 		this.colour_templates = ["Lime","cyan","darkorange","Red","Green","#0000FF"];
 		this.controls_templates = [
 		["ArrowUp","ArrowRight","ArrowDown","ArrowLeft","1","2"]
 		,["w","d","s","a","f","g"]
 		,["y","j",'h','g','k','l']
 		];
-
-		}
-
-	draw(){this.panels.forEach(function(panel){panel.draw();});}
+	}
 
 
-	addPanel(panel)
-		{
-		this.panels.push(panel);
+	main(){
+		this.draw();
+	}
+
+
+	draw(){
+		this.current_panels.forEach(function(tank_panel){tank_panel.draw();});
+	}
+
+
+	addTankPanel(tank_panel){
+		this.tank_panels.push(tank_panel);
+		this.current_panels = [this.start_panel].concat(this.tank_panels);
 		this.updatePanelHorizontals();
-		}
+	}
 
 
-	removePanel(panel)
-		{
-		this.panels.splice(this.panels.indexOf(panel),1);
+	removeTankPanel(tank_panel){	
+		this.tank_panels.splice(this.tank_panels.indexOf(tank_panel),1);
+		this.current_panels = [this.start_panel].concat(this.tank_panels);
 		this.updatePanelHorizontals();
-		}
+	}
 
 
-	updatePanelHorizontals()
-		{
-		var len = this.panels.length;
-		var width = canvas.width/(len-1); 
-		for(var i=1;i<len;i++)
+	updatePanelHorizontals(){
+		//Set border width of start_panel
+		if(this.tank_panels.length==0)
 			{
-			var panel = this.panels[i];
-			panel.x=(i-1)*width;
-			panel.width=width;
-			panel.buttons.forEach(function(e){e.update()});
+			this.start_panel.east_border=PREGAME_BORDER_WIDTH;
+			return;
 			}
-		}
+		this.start_panel.east_border=PREGAME_BORDER_WIDTH/2;
+	
+		//Set width and border widths and x values of tank_panels
+		var width = (canvas.width-this.start_panel.width)/(this.tank_panels.length); 
+		for(var i=0;i<this.tank_panels.length;i++)
+			{	
+				var tank_panel = this.tank_panels[i];
+				tank_panel.x=this.start_panel.width +(i)*width;
+				tank_panel.width=width;
+				tank_panel.buttons.forEach(function(b){b.update()});
+				tank_panel.east_border=PREGAME_BORDER_WIDTH/2;
+					
+			}
+		this.tank_panels[this.tank_panels.length-1].east_border=PREGAME_BORDER_WIDTH;
+	}
+
+
 
 	//Look through all elements to see if any of them were at the click location
-	onclick(x,y)
-		{
-		this.panels.forEach(function(panel)
+	onclick(x,y){
+		this.button_has_been_found=false; //local var to stop looking if we found our button
+		this.current_panels.forEach(function(panel)
 			{
+			if(!this.button_has_been_found)
 			panel.buttons.forEach(function(button)
 				{
-		
+					if(!this.button_has_been_found)
 					if(doRectsOverlap([x,y,1,1],button.get_as_Rect()))
 						{
+						this.button_has_been_found=true;
 						button.onclick();
-						return;
 						}
-				});
-			});
-		}
+				}.bind(this));
+			}.bind(this));
+	}
 
-	addPanelToMakePanels()
-		{
-		var panel = new Panel('red');
-		var add_button = new Button(panel,0,0,"Add Tank");
-		add_button.onclick = function(){
-			var current_template = pregame.panels.length-2;
-			pregame.addPanel(new TankPanel(pregame.colour_templates[current_template],pregame.controls_templates[current_template]));
-			};
-		add_button.y=add_button.panel.height/2-add_button.height/2;
-		add_button.text="Add Tank";
-		panel.buttons=[add_button];
-		this.addPanel(panel);
-		}
 
-	addStartPanel()
-		{
-		var start_panel = new Panel("blue");
-		start_panel.y=this.height*4/5;
-		start_panel.height=this.height*1/5;
-		start_panel.width = canvas.width;
-		this.addPanel(start_panel);
-
-		var start_button = new Button(start_panel,0,0,"Start");
-		start_button.onclick = this.start.bind(this);
-		start_button.center_horizontally();
-		start_button.center_vertically();
-		start_panel.addButton(start_button);
-		}
-
-	handleKeydown(key){
+	keyDownHandler(e){
 		if(this.focus==null){return;}
-		this.focus.handleKeydown(key);
-		}
-
-	start()
-		{
-		var maze = new Maze(5,7,canvas.width,canvas.height*4/5,wall_thiccness);
-	
-		//Instantiate tanks from the input in the panels 
-		for(var i=2;i<this.panels.length;i++)
-			{
-			var panel = this.panels[i];
-			var cb = this.panels[i].buttons;
-			var controls = [cb[1].key, cb[2].key, cb[3].key, cb[4].key, cb[5].key, cb[6].key];
-			var rnd_pos = maze.getRandomSquare().getCenter();
-		
-			var tank = new Tank (0,0,maze,controls,panel.colour);
-			maze.placeObject(tank);
-			}
-
-		
-		main_object = maze;
-		}
+		this.focus.keyDownHandler(e);
 	}	
+}	
 
 
 //Panel to change controls
-class Panel
-	{
-
-	constructor(colour)
-		{
-		this.width = 0;
-		this.height = canvas.height*4/5;
+class Panel{
+	constructor(colour){
+		this.width = canvas.width/5; //this gets overwritten right away by the calling code, so initial value doesn't matter
+		this.height = canvas.height;
 		this.x=0;
 		this.y=0;
 		this.colour = colour;
-		this.lineWidth=PREGAME_BORDER_WIDTH;
 		this.buttons=[];
-		}
 
-	addButton(button)
-		{
-		this.buttons.push(button);
-		}
-
-	draw()
-		{
-		ctx.fillStyle = this.colour;
-		ctx.fillRect(this.x,this.y,this.width,this.height);
-
-		ctx.strokeStyle = "black";
-		ctx.lineWidth=this.lineWidth;
-		ctx.strokeRect(this.x,this.y,this.width-3,this.height)
-		this.buttons.forEach(function(b){b.draw()});	
-		}
-	
+		this.north_border=PREGAME_BORDER_WIDTH;
+		this.east_border=PREGAME_BORDER_WIDTH/2;
+		this.south_border=PREGAME_BORDER_WIDTH;
+		this.west_border=PREGAME_BORDER_WIDTH/2;
 	}
 
 
-class Button
-	{
-	constructor(panel,x,y,text="")
-		{
+	addButton(button){
+		this.buttons.push(button);
+	}
+
+
+	draw(){
+	ctx.fillStyle = "black";
+	ctx.fillRect(this.x,this.y,this.width,this.height);
+
+	ctx.fillStyle = this.colour;
+	ctx.fillRect(this.x+this.west_border, this.y+this.north_border,this.width-this.west_border-this.east_border,this.height-this.north_border-this.south_border);
+	
+	this.buttons.forEach(function(b){b.draw()});	
+	}
+}
+
+
+class StartPanel extends Panel {
+ 	constructor(colour,pregame){
+	 	super(colour);
+		this.pregame=pregame;
+		this.west_border=PREGAME_BORDER_WIDTH;
+		this.east_border=PREGAME_BORDER_WIDTH;
+
+		var start_button = new Button(this,0,0,"Start");
+		start_button.onclick = this.start.bind(this.pregame);
+		start_button.center_horizontally();	
+		start_button.y = canvas.height * 9/20;
+		this.addButton(start_button);
+
+		var add_button = new Button(this,0,0,"Add Tank");
+		add_button.onclick = function(){
+			var pregame=this.panel.pregame;
+			var current_template = pregame.tank_panels.length;
+			pregame.addTankPanel(new TankPanel(pregame,pregame.colour_templates[current_template],pregame.controls_templates[current_template]));
+		};
+		add_button.y=canvas.height * 11/20;
+		add_button.center_horizontally();
+		this.addButton(add_button);
+
+		var settings_button = new Button(this,0,0,"Settings");
+		settings_button.onclick = function(){
+				this.pregame.current_panels=[this.pregame.settings];
+		}.bind(this);
+		settings_button.y=canvas.height * 13/20;
+		settings_button.center_horizontally();
+		this.addButton(settings_button);
+	  }
+
+
+
+
+	//Starts the game by making a maze and populating it with tanks based on tank_panel attributes
+ 	start(){
+
+		var maze = new Maze(this.game);
+		this.game.main_object=maze;
+
+		//Instantiate tanks from the input in the panels 
+		for(var i=0;i<this.tank_panels.length;i++){
+			var panel = this.tank_panels[i];
+			var cb = panel.buttons;
+			var controls = [cb[1].value, cb[2].value, cb[3].value, cb[4].value, cb[5].value, cb[6].value];
+			var rnd_pos = maze.getRandomSquare().getCenter();
+			
+			var tank = new Tank (0,0,maze,controls,panel.colour);
+			maze.placeObject(tank);
+		}
+	}
+}
+
+
+class Button{
+	constructor(panel,x,y,text=""){
 		this.height = 20;
+		this.width=panel.width/1.5;
+		this.x=0;
 		this.y=y;
 		this.text=text;
 		this.panel=panel;
-		}
+	}
 
-	get_as_Rect()
-		{
+
+	get_as_Rect(){
 		return [this.x,this.y,this.width,this.height];
-		}
+	}
 	
+
 	//Called when a new panel is added, and things need to get resized
-	update()
-		{
-		this.width=this.panel.width/1.2;
-		this.x=this.panel.x+this.panel.width/2-this.width/2;
-		}
-	
-	draw()
-		{
-		ctx.fillStyle = "white";
-		ctx.fillRect(this.x,this.y,this.width,this.height);
-		ctx.fillStyle = "black";
-		ctx.textAlign = "center";
-		ctx.font = "10px Arial";
-		ctx.fillText(this.text,this.x+this.width/2,this.y+this.height/1.5);
-		}
-
-	center_horizontally()
-		{
-		this.width=this.panel.width/2;
-		this.x=this.panel.x+this.panel.width/2-this.width/2;
-		}
-
-	center_vertically()
-		{
-		this.height=this.panel.height/2;
-		this.y=this.panel.y+this.panel.height/2-this.height/2;
-		}
+	update(){
+		this.resize_horiontals();
 	}
 
 
-class TankPanel extends Panel 
-	{
-	constructor(colour="green",assignment=undefined)
-		{
-		super(colour);
-		var delete_button = new Button(this,0,this.height/12,"delete");
-		delete_button.onclick = function(){
-			var index = pregame.panels.indexOf(this);
-			pregame.panels.splice(index,1);
-			pregame.updatePanelHorizontals();
-			}
-		this.addButton(delete_button);
-		delete_button.center_horizontally();
-
-
-		var controls = ["up","right","down","left","attack","special"];
-		for (var i=0;i<controls.length;i++)
-			{
-			var button = new setControlsButton(this,0,this.height*(3+i)/12,controls[i],assignment ? assignment[i] : undefined);
-			button.center_horizontally();
-			this.addButton(button);
-			}
-		}
+	resize_horiontals(){
+		this.width=this.panel.width/1.5;
+		this.x=this.panel.x+this.panel.width/2-this.width/2;
 	}
-
-
-class setControlsButton extends Button 
-	{
-	constructor(panel,x,y,text,key=undefined)
-		{
-		super(panel,x,y,text+":");
-		this.control=text+": "; //e.g. "attack"
-		this.key = key; //e.g. spacebar	
-		}
-
-	handleKeydown(key)
-		{
-		 this.key=key;
-		 this.text = this.control + key;
-		}
-
-	onclick()
-		{
-		pregame.focus = this;
-		}
 	
-	draw()
-		{
-		if(pregame.focus == this)
-			{
+
+	draw(){
+		if(this.panel.pregame.focus == this){
 			ctx.fillStyle = "red";
 			ctx.fillRect(this.x,this.y,this.width,this.height);	
-			}
-
-		else
-			{
+		}
+		else{
 			ctx.fillStyle = "white";
 			ctx.fillRect(this.x,this.y,this.width,this.height);	
-			}
+		}
 
 		ctx.fillStyle = "white";
 		ctx.fillRect(this.x+2,this.y+2,this.width-4,this.height-4);
-			
+				
 		ctx.fillStyle = "black";
 		ctx.textAlign = "center";
 		ctx.font = "10px Arial";
-		ctx.fillText(this.key ? this.control + this.key : this.control ,this.x+this.width/2,this.y+this.height/1.5);	
-		
+
+		this.fill_text()
+	}
+
+
+	fill_text(){
+		ctx.fillText(this.text.toString(),this.x+this.width/2,this.y+this.height/1.5);
+	}
+
+	center_horizontally(){
+		this.width=this.panel.width/2;
+		this.x=this.panel.x+this.panel.width/2-this.width/2;
+	}
+
+	center_vertically(){
+		this.y=this.panel.y+this.panel.height/2-this.height/2;
+	}
+}
+
+
+class TankPanel extends Panel {
+	constructor(pregame,colour="green",assignment=undefined){
+		super(colour);
+	
+		this.pregame=pregame;
+		var delete_button = new Button(this,0,this.height/12,"delete");
+		delete_button.onclick = function(){
+			this.pregame.removeTankPanel(this);
+		}.bind(this);
+		this.addButton(delete_button);
+		delete_button.center_horizontally();
+
+		var controls = ["up","right","down","left","attack","special"];
+		for (var i=0;i<controls.length;i++){
+			var button = new SetControlsButton(this,0,this.height*(3+i)/12,controls[i],assignment ? assignment[i] : undefined);
+			button.center_horizontally();
 		}
 	}
+}
+
+
+class SetControlsButton extends Button {
+	constructor(panel,x,y,text,default_value=""){
+ 		super(panel,x,y,text+": ");
+		this.control=text+": "; //e.g. "attack"
+		this.value = default_value; //e.g. spacebar	
+		this.panel.addButton(this);
+	}
+
+	keyDownHandler(key){
+		this.value=key;
+	}
+
+	onclick(){
+		this.panel.pregame.focus = this;
+	}
+	
+	fill_text(){
+		var text = this.text.toString() + this.value.toString();
+		ctx.fillText(text,this.x+this.width/2,this.y+this.height/1.5);
+	}
+}
+
+class SetSettingsButton extends SetControlsButton {
+
+	constructor(panel,x,y,text,default_value="",attribute_name){
+	 	super(panel,x,y,text,default_value);
+	 	this.attribute_name=attribute_name;
+	 	this.value=this.panel.pregame.game[attribute_name].toString();
+	 }
+
+	keyDownHandler(key){
+		if(key=="Backspace"){this.value=this.value.slice(0,-1);return;}
+		if(key=="Enter"){this.panel.pregame.focus = this.panel.back;return;}
+		this.value+=key;
+	}
+
+
+	onclick(){
+		this.panel.pregame.focus = this;
+		this.value="";
+	}
+
+	fill_text(){
+		if(!this.value)	{var text = this.text.toString();} 
+		else 		 	{var text = this.text.toString() + this.value.toString();}
+		ctx.fillText(text,this.x+this.width/2,this.y+this.height/1.5);
+	}
+
+}
+
+class SettingsPanel extends Panel{
+
+	constructor(pregame){
+		super("Green");
+		this.width=canvas.width;
+		this.pregame=pregame;
+	
+		//Create settings buttons
+		this.settings = [
+		 ["Number of Rows","num_of_rows"]
+		,["Number of Columns","num_of_columns"]
+		,["Movement Speed","move_speed"]
+		,["Friendly Fire","friendly_fire"]
+		,["Number of Bullets","bullet_limit"]
+		,["Time Between Powerups (s)","powerup_interval"]
+		,["Max powerups on screen","powerup_limit"]
+		,["Duration of powerups (s)","powerup_duration"]
+		];
+		this.settings.forEach(function(ar){this.make_button(ar)}.bind(this));
+		this.addBackButton();
+
+		//Position the buttons on the screen
+		var blen = this.buttons.length;
+		for(var i=0;i<blen;i++){
+			var button = this.buttons[i];
+			button.y = canvas.height*4/5/(blen-1)*(i)+button.height;
+			button.resize_horiontals();
+		}
+	}
+
+	addBackButton(){
+		var back = new Button(this,0,0,"Back");
+		this.back=back;
+		back.y=canvas.height*5/6;
+		back.update();
+		back.onclick = function(){
+			var save_successful = this.save();
+			if (!save_successful){return;}
+			this.pregame.current_panels = [this.pregame.start_panel].concat(this.pregame.tank_panels);
+		}.bind(this);
+		back.keyDownHandler = function(key){
+			if (key=="Enter" && this.panel.pregame.focus==this){this.onclick();}
+		}
+		this.addButton(back);
+	}
+
+	//If save is successful return true, else return false 
+	save(){
+		var back_button = this.back;
+		var buttons_that_must_be_posints=[0,1,4,6];
+		var buttons_that_must_be_posnumbers=[2,5,7];
+			
+		for(var i=0;i<buttons_that_must_be_posints.length;i++){
+			var button = this.buttons[buttons_that_must_be_posints[i]];
+			if (!this.isPosInt(button.value)){
+				back_button.text="x must be positive Integer".replace("x",button.text).replace(":","");
+				return false;
+			}
+			button.panel.pregame.game[button.attribute_name] = button.value;
+		}
+
+		for(var i=0;i<buttons_that_must_be_posnumbers.length;i++){
+			var button = this.buttons[buttons_that_must_be_posnumbers[i]];
+			if (!this.isPosNumber(button.value))
+				{
+					back_button.text="x must be positive Integer".replace("x",button.text).replace(":","");
+					return false;
+				}
+		}
+
+		var friendly_fire_button = this.buttons[3];
+		this.pregame.game.friendly_fire = (friendly_fire_button.value=="true" ? true : false)
+
+		//Return to main screen and reset text of back_button
+		this.pregame.current_panels = [this.pregame.start_panel].concat(this.pregame.tank_panels);
+		back_button.text="Back";
+	}
+
+
+	isPosInt(str){
+		if(isNaN(str)){return false;}
+		var number = parseFloat(str);
+		if(!Number.isInteger(number) || number<0){return false;}
+		return true;
+	}
+
+
+	isPosNumber(str){
+		if(isNaN(str)){return false;}
+		var number = parseFloat(str);
+		if(number<=0){return false;}
+		return true;
+	}
+
+
+	make_button(ar){	
+		//Make button a property of SettingsPanel and set its keydown and onclick
+		var text = ar[0];
+		var attribute_name = ar[1];
+		this[attribute_name]=new SetSettingsButton(this,0,0,text,"",attribute_name);
+			
+		//Corner case for friendly fire button 
+		if(ar[0]=="Friendly Fire"){
+			this[attribute_name].onclick = function(){
+				this.panel.pregame.focus=this;
+				this.value=="true" ? this.value="false":this.value="true"}
+			}
+		}
+}
--- js/Tank.js
+++ js/Tank.js
@@ -1,12 +1,9 @@
 //ctx must be defined
 
-class Tank
-{
-
+class Tank{
 
 	//controls = [up,right,down,left,fire,special] e.g. ['a',"ArrowRight"]
-	constructor(x,y,maze,controls,colour="black")
-		{
+	constructor(x,y,maze,controls,colour="black"){
 		this.x=x;
 		this.y=y;
 		this.controls=controls;
@@ -18,11 +15,13 @@ class Tank
 		this.height=maze.height/maze.num_of_rows/3;
 		this.rotation=0; //pointing straight up
 		this.bullets=[];
+		this.bullet_limit=BULLET_LIMIT;
 		this.score=0;
 		this.is_dead=false;
+		
 
 		var o=this.maze.squares[0][0];
-		this.move_speed=MOVE_SPEED *Math.min(o.width,o.height)/60;
+		this.move_speed=maze.game.move_speed*Math.min(o.width,o.height)/60;
 		this.rotation_speed=ROTATION_SPEED;
 		
 
@@ -37,16 +36,23 @@ class Tank
 		document.addEventListener("keyup", this.keyUpHandler.bind(this), false);
 		document.addEventListener("keypress",this.keyPressHandler.bind(this),false);
 
-		}
+	}
 
-
-	draw()
-		{
-		//Draw and progress bullets. IF tank is alive THEN draw and progress tank.
-		this.handleBullets();
-		this.bullets.forEach(function(e){e.draw();})
+	main() {
 		if(this.is_dead){return;}
+
+		if(this.shouldFire()){ this.fire(this.rotation, BULLET_SPEED); }
+		this.shooting=false;
+
+		this.bullets.forEach(function(bullet){bullet.main();})
+
 		this.handleMovement();
+
+		this.draw();
+	}
+
+	draw(){
+		this.bullets.forEach(function(e){e.draw();})
 		
 		//Drawing
 		ctx.save();
@@ -54,39 +60,43 @@ class Tank
 		ctx.rotate(this.rotation);
 
 		//drawing with img loaded instead of tank
-		if(this.hasOwnProperty("img"))
-			{
+		if(this.hasOwnProperty("img")){
 			ctx.drawImage(this.img,-this.width/2,-this.height/2,this.width,this.height);
-			}
+		}
 
 		else{
 			ctx.fillStyle=this.colour;
 			ctx.fillRect(-this.width/2,-this.height/2,this.width,this.height);
 			ctx.fillRect(-this.width/10,0,this.width/5,-this.height/1.2)
-			}
-			ctx.restore();
+		}
+		ctx.restore();
 		
-		}
+	}
 	
+	shouldFire(){
+		if (this.shooting && this.bullets.length<this.bullet_limit) {return true;}
+		else {return false;}
+	
+	}
 
-	handleBullets()
-		{
-		if(this.shooting){
-			this.shooting=false;
-			var x_vel = BULLET_SPEED*Math.sin(this.rotation);
-			var y_vel = BULLET_SPEED*-Math.cos(this.rotation);
-			//x,y _pos are at tip of the cannon
-			var x_pos = this.x+this.width/2 + Math.sin(this.rotation)*this.height;
-			var y_pos = this.y+this.height/2 + Math.cos(this.rotation)*-this.height;
-			var new_bullet = new Bullet(this,[x_vel,y_vel],x_pos,y_pos);
-			this.bullets.push(new_bullet);
-			}
-		}
+	fire(rotation,speed){
+		this.fire_helper(rotation,speed);
+	}
+
+	fire_helper(rotation, speed){
+
+		var x_vel = speed*Math.sin(rotation);
+		var y_vel = speed*-Math.cos(rotation);			//x,y _pos are at tip of the cannon
+		var x_pos = this.x+this.width/2 + Math.sin(this.rotation)*this.height;
+		var y_pos = this.y+this.height/2 + Math.cos(this.rotation)*-this.height;
+		var new_bullet = new Bullet(this,[x_vel,y_vel],x_pos,y_pos);			
+		this.bullets.push(new_bullet);
+			
+	}
 	
 
 	//Called every iteration of draw. Checks to see if buttons are pressed and moves tank accordingly.
-	handleMovement()
-		{
+	handleMovement(){
 		var x=this.x;
 		var y=this.y;
 		var ms=this.move_speed;
@@ -104,60 +114,52 @@ class Tank
 			this.tryMovingTo([this.x,this.y+this.move_speed*Math.cos(this.rotation)]);
 			}
 		if(this.leftPressed){this.rotation-=this.rotation_speed;}
-		}
+	}
 	
 
 	//Helper for handleMovement(). Tries to move the tank to x,y. Will fail if maze.doesRectCollide(tank) returns false.
-	tryMovingTo(pos)
-		{
+	tryMovingTo(pos){
 		var x = pos[0];
 		var y = pos[1];
 
-		if(!this.maze.doesRectCollide([x,y,this.width,this.height]))
-			{
+		if(!this.maze.doesRectCollide([x,y,this.width,this.height])){
 			this.x=x;
 			this.y=y;
-			}
 		}
+	}
 	
 
-	keyPressHandler(e)
-		{
+	keyPressHandler(e){
 		if(e.key == this.controls[4]){this.shooting=true;}
-		}
+	}
 	
 
-	keyDownHandler(e)
-		{
-	
+	keyDownHandler(e){
+
 		if(e.key == this.controls[0]){this.upPressed=true;}
 		if(e.key == this.controls[1]){this.rightPressed=true;}
 		if(e.key == this.controls[2]){this.downPressed=true;}
 		if(e.key == this.controls[3]){this.leftPressed=true;}
-
-		}
+	}
 	
 
-	keyUpHandler(e)
-		{
+	keyUpHandler(e){
 
 		if(e.key == this.controls[0]){this.upPressed=false;}
 		if(e.key == this.controls[1]){this.rightPressed=false;}
 		if(e.key == this.controls[2]){this.downPressed=false;}
 		if(e.key == this.controls[3]){this.leftPressed=false;}
 
-		}
+	}
 	
 
-	loadImage(img_element)
-		{
+	loadImage(img_element){
 		this.img=img_element;
-		}
+	}
 	//Legacy code, may be used for buffs/debuffs
 	
 
-	originalMovement()
-		{
+	originalMovement(){
 		var x=this.x;
 		var y=this.y;
 		var ms=this.move_speed;
@@ -166,14 +168,31 @@ class Tank
 		if(this.rightPressed){this.tryMovingTo([this.x+this.move_speed,this.y]);}
 		if(this.downPressed){this.tryMovingTo([this.x,this.y+this.move_speed]);}
 		if(this.leftPressed){this.tryMovingTo([this.x-this.move_speed,this.y]);}
-		}
+	}
 	
+	onBulletHit(){
+		this.destroy();
+	}
 
-	destroy()
-		{
+	destroy(){
 		this.is_dead=true;
 		this.maze.tankDestroyed();
+	}
+
+	restart(){
+
+		this.is_dead=false;
+		var pos = this.maze.getRandomSquare().getCenter();
+		this.x=pos[0];
+		this.y=pos[1];
+		this.bullets=[];
+		this.shooting=false;
+
+		//Restart powerups
+		if(this.powerup_timeout!=undefined){
+			this.undo_powerup();
+			clearTimeout(tank.powerup_timeout);
+			this.powerup_timeout=undefined;
 		}
-
-
+	}
 }
