28ecc449d615e09a300b21e2d38a229059767bae
Thomas <thomaskosa24@gmail.com>
2019-12-08 15:39:57 -0600

    removed old tanks folder

--- download.js
+++
@@ -1,7 +0,0 @@
-var download = function (url) {
-	var a= document.createElement("a");
-	a.href=url;
-	a.download=url;
-	a.click()
-}
-$$(".has_imageurl").forEach(function(ele){url=ele.getAttribute("data-imageurl");if(url.endsWith("jpg")||url.endsWith("png")){download(url)}});
